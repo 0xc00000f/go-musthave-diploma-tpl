e8f790561328e063930f7f37f97c19ed452ab504
Maksim Ciganov <m.ciganov@gaijin.team>
2023-01-16 00:29:30 +0600

    init go mod add gin add running server

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,13 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"github.com/0xc00000f/go-musthave-diploma-tpl/cmd/gophermart/handlers"
+)
+
+func main() {
+	router := handlers.NewRouter()
+
+	log.Fatal("http server down", router.Run())
+}
