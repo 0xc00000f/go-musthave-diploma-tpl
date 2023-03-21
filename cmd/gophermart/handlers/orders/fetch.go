0ff7bb58d6b0c7dc2131f3a75cc763fc86e9a42d
Maxim Tsyganov <0xc00000f@gmail.com>
2023-03-22 02:19:40 +0600

    fix fetch order handler

--- cmd/gophermart/handlers/orders/fetch.go
+++ cmd/gophermart/handlers/orders/fetch.go
@@ -4,11 +4,12 @@ import (
 	"context"
 	"net/http"
 	"sort"
+	"time"
 
 	"github.com/gin-gonic/gin"
 
 	"github.com/0xc00000f/go-musthave-diploma-tpl/cmd/gophermart/storage"
-	"github.com/0xc00000f/go-musthave-diploma-tpl/cmd/gophermart/structures"
+	"github.com/0xc00000f/go-musthave-diploma-tpl/cmd/gophermart/structures/status"
 )
 
 type Fetcher interface {
@@ -16,7 +17,13 @@ type Fetcher interface {
 	FetchByUser(ctx context.Context, username string) (storage.OrderDataMap, error)
 }
 
+type FetchResp []Order
+
 type Order struct {
+	OrderNumber string             `json:"number"`
+	Status      status.OrderStatus `json:"status"`
+	Accrual     int64              `json:"accrual,omitempty"`
+	CreatedTS   string             `json:"uploaded_at"`
 }
 
 func FetchOrder(fetcher Fetcher) func(*gin.Context) {
@@ -40,11 +47,16 @@ func FetchOrder(fetcher Fetcher) func(*gin.Context) {
 	}
 }
 
-func respFromOrderDataMap(orders storage.OrderDataMap) []structures.Order {
-	var resp []structures.Order //nolint:prealloc
+func respFromOrderDataMap(data storage.OrderDataMap) FetchResp {
+	var resp FetchResp //nolint:prealloc
 
-	for _, order := range orders {
-		resp = append(resp, structures.OrderFromStorageData(*order))
+	for _, order := range data {
+		resp = append(resp, Order{
+			OrderNumber: order.OrderNumber,
+			Status:      order.Status,
+			Accrual:     order.Accrual,
+			CreatedTS:   time.Unix(order.CreatedTS, 0).Format(time.RFC3339),
+		})
 	}
 
 	sort.Slice(resp, func(i, j int) bool {
